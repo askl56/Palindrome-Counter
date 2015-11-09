0f27b4e8f66c39ce590420c3dbbf286e4bb4c03c
askl56 <amscott@openmailbox.org>
2015-11-09 21:43:07 +0000

    Added a basic test boilerplate

---
+++ spec/counter_spec.rb
@@ -0,0 +1,23 @@
+require 'counter'
+
+describe 'Counter' do
+
+  subject(:counter) { Counter.new }
+
+  it "outputs the 'File checked' output" do
+    expect { subject.call }.to output(/File checked: test_files\/another-test.txt/).to_stdout
+  end
+
+  it 'outputs the "Range" to output' do
+    expect { subject.call }.to output(/Range: 1..100/).to_stdout
+  end
+
+  it 'outputs the palindrome count to output' do
+    expect { subject.call }.to output(/Palindromes: 18/).to_stdout
+  end
+
+  it 'outputs the new line separator to output' do
+    expect { subject.call }.to output(/\========================/).to_stdout
+  end
+
+end
