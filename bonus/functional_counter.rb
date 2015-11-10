f1473e7b66b8eb53bb4db5ab3370748bcc852144
askl56 <amscott@openmailbox.org>
2015-11-10 00:07:29 +0000

    fixed dodgy directory in functional solution

--- bonus/functional_counter.rb
+++ bonus/functional_counter.rb
@@ -2,7 +2,7 @@ open_file         = -> (path) { File.open(path) { |file| { path: path, range: fi
 to_range          = -> (data) { data[:range] = Range.new(data[:range].first.to_i, data[:range].last.to_i); data }
 count_palindromes = -> (data) { data[:count] = data[:range].select { |number| number.to_s == number.to_s.reverse }.length; data }
 
-data = Dir['*.txt'].map(&open_file)
+data = Dir['test_files/*.txt'].map(&open_file)
                    .map(&to_range)
                    .map(&count_palindromes)
 
