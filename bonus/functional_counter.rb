705db0d8e54819ae3ec08b5d06cd06fc0f25c8af
askl56 <amscott@openmailbox.org>
2015-11-10 00:07:53 +0000

    fixed spacing

--- bonus/functional_counter.rb
+++ bonus/functional_counter.rb
@@ -3,8 +3,8 @@ to_range          = -> (data) { data[:range] = Range.new(data[:range].first.to_i
 count_palindromes = -> (data) { data[:count] = data[:range].select { |number| number.to_s == number.to_s.reverse }.length; data }
 
 data = Dir['test_files/*.txt'].map(&open_file)
-                   .map(&to_range)
-                   .map(&count_palindromes)
+                              .map(&to_range)
+                              .map(&count_palindromes)
 
 data.each do |element|
   puts "File checked: #{element[:path]}"
